f3e49eb2e2cc08159fcb3b3403589043897359d7
Alex Grytsai <a.grytsai1987@gmail.com>
2025-05-27 17:43:50 +0300

    feat: Implement main function for drone position calculation
    This commit introduces the `main` function, which orchestrates the calculation of the drone's position based on control points and the drone's azimuth. It leverages the `GeoService` to perform the calculation.
    
    The script also includes a conditional block to execute the `main` function and print the calculated coordinates when the script is run directly. This allows for easy testing and demonstration of the drone position calculation functionality.

---
+++ main.py
@@ -0,0 +1,19 @@
+from src.entity import Drone, GeoPoint, ImagePoint
+from src.services import GeoService
+
+
+def main(
+    drone: Drone = Drone(),
+    geo_service: GeoService = GeoService(),
+    control_geo_point: GeoPoint = GeoPoint(50.603694, 30.650625),
+    control_image_point: ImagePoint = ImagePoint(558, 328),
+) -> GeoPoint:
+    return geo_service.calculate_drone_position(
+        control_geo_point,
+        drone.azimuth,
+        control_image_point,
+    )
+
+
+if __name__ == "__main__":
+    print(f"Координати Центру зображення: {main()}")
