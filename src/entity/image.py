995cb3c71b10b97a53e06e6cd2b02dfecea879f1
Alex Grytsai <a.grytsai1987@gmail.com>
2025-05-27 17:23:57 +0300

    feat: Add Image entity with center property
    This commit introduces the `Image` entity, which represents an image with width and height attributes. It also includes a `center` property that calculates and returns the image's center coordinates as an `ImagePoint` object. This is a foundational component for further image processing and analysis functionalities within the drone application.

---
+++ src/entity/image.py
@@ -0,0 +1,24 @@
+from point import ImagePoint
+
+
+class Image:
+    """
+    Клас, що представляє зображення з шириною та висотою.
+    """
+
+    def __init__(self, width: int = 640, height: int = 512) -> None:
+        self.width = width
+        self.height = height
+
+    @property
+    def center(self) -> ImagePoint:
+        """
+        Властивість, що повертає центр зображення.
+
+        Повертає:
+            ImagePoint: координати центра зображення
+        """
+        return ImagePoint(int(self.width / 2), int(self.height / 2))
+
+    def __str__(self) -> str:
+        return f"Image({self.width}x{self.height})"
